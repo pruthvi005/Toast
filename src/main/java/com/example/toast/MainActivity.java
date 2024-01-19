a6b382c3a68063b2e18c19d0cfac6f9dae20dd18
pruthvi005 <155711701+pruthvi005@users.noreply.github.com>
2024-01-19 13:20:37 +0530

    Add files via upload

---
+++ src/main/java/com/example/toast/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.Button;
+import android.view.View;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button=findViewById(R.id.b1);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(MainActivity.this,"You Clicked Toast....",Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+}
